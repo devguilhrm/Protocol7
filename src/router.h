803a9e35f76371d36f157aace48fc4a6a93bb5b8
devguilhrm <guilimadev19@gmail.com>
2026-08-02 19:05:18 -0300

    v1

--- src/router.h
+++ src/router.h
@@ -1,16 +1,8 @@
-#ifndef PARSE_H
-#define PARSE_H
+#ifndef ROUTER_H
+#define ROUTER_H
 
-#include "config.h"
+#include "parse.h"
 
-#define REQ_BUF_SIZE 8192
-
-typedef struct {
-    char method[16];
-    char uri[MAX_PATH_LEN];
-    char host[MAX_PATH_LEN];
-} HttpRequest;
-
-int parse_request(const char* buf, size_t len, HttpRequest* req);
+int route_request(const HttpRequest* req, const Config* cfg, char* out_path);
 
 #endif
